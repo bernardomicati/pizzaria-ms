1c81ae338ba4da0a2fc6514997ed7f6f45ed504c
Bernardo Micati <114197114+bernardomicati@users.noreply.github.com>
2023-03-20 18:54:03 -0300

    Delete pizarria-ms/pizzaria-ms directory

--- pizarria-ms/pizzaria-ms/pedidos/src/main/java/br/com/tech4me/pedidos/repository/PedidoRepository.java
+++
@@ -1,8 +0,0 @@
-package br.com.tech4me.pedidos.repository;
-
-import br.com.tech4me.pedidos.model.Pedido;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface PedidoRepository extends MongoRepository<Pedido,String>{
-    
-}
--- pizarria-ms/pizzaria-ms/pizzaria/src/main/java/br/com/tech4pizza/pizzaria/repository/PizzaRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.tech4pizza.pizzaria.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import br.com.tech4pizza.pizzaria.model.Pizza;
-
-public interface PizzaRepository extends MongoRepository<Pizza, String> {
-  
-}
